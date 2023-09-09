a21e32bf8e2414e47a6b6d2d9eebcbf3efbf34dc
rusheek4112 <117939047+rusheek4112@users.noreply.github.com>
2023-09-09 11:39:48 +0530

    Update app2.js

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-function talk(){
-var know = {
-"Who are you" : "Hello, Codewith_random here ",
-"How are you" : "Good :)",
-"What can i do for you" : "Please Give us A Follow & Like.",
-"Your followers" : "I have my family of 5000 members, i don't have follower ,have supportive Famiy ",
-"ok" : "Thank You So Much ",
-"Bye" : "Okay! Will meet soon.."
-};
-var user = document.getElementById('userBox').value;
-document.getElementById('chatLog').innerHTML = user + "<br>";
-if (user in know) {
-document.getElementById('chatLog').innerHTML = know[user] + "<br>";
-}else{
-document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";
+function searchDuckDuckGo(query) {
+  const apiKey = '421a10a440615a4ccea08d6d4cadb8541608ace6749fca6ea32305278fabf77f'; // Replace 'YOUR_NEW_API_KEY' with your actual API key
+  const apiUrl = `https://api.duckduckgo.com/?q=${encodeURIComponent(query)}&format=json&apiKey=${apiKey}`;
+
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Extract and return the relevant information from the API response
+      const results = data.AbstractText;
+      // Process and display the results as desired
+
+      return results;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      return null;
+    });
 }
+
+// Function to handle the user's query and perform the search
+function talk() {
+  const userQueryInput = document.getElementById('userbox');
+  const userQuery = userQueryInput.value;
+
+  searchDuckDuckGo(userQuery)
+    .then(response => {
+      if (response) {
+        if (response === '') {
+          console.log("Sorry, I couldn't find any relevant information.");
+          const chatlog = document.getElementById('chatLog');
+          chatlog.textContent = "Sorry, I couldn't find any relevant information.";
+        } else {
+          console.log(response);
+          const chatlog = document.getElementById('chatLog');
+          chatlog.textContent = response; // Update the chatlog with the response
+        }
+      } else {
+        console.log("Sorry, there was an error retrieving information.");
+        const chatlog = document.getElementById('chatLog');
+        chatlog.textContent = "Sorry, there was an error retrieving information.";
+      }
+    });
 }
